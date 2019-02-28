86393fe0bf7788443003b844144afc7ed9c1f907
Anwesh43 <anweshthecool0@gmail.com>
2019-02-28 11:01:36 +0530

    adding LineExpandParallel class

--- LineExpandParallel.ts
+++ LineExpandParallel.ts
@@ -178,3 +178,27 @@ class LEPNode {
         return this
     }
 }
+
+class LineExpandParallel {
+
+    root : LEPNode = new LEPNode(0)
+    curr : LEPNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
